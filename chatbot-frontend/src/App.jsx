07b7ea0075c05cb9a24d04b342d3e36fd84a686c
abc <abc@example.com>
2024-10-22 16:18:48 +0100

    Fix the Authentications

--- chatbot-frontend/src/App.jsx
+++ chatbot-frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-// import Pricing from "./components/Pricing";
-import Layout from "./Layout";
+import MainLayout from "./Layout";
 import Home from "./components/Home";
 import SignIn from "./Auth/SingIn";
 import SignUp from "./Auth/SignUp";
@@ -8,22 +7,19 @@ import axios from "axios";
 import { UserContextProvider } from "./UserContext";
 import { ToggleProvider } from "./components/store/ToggleContext";
 
-
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
-
 
 function App() {
   return (
     <ToggleProvider>
     <UserContextProvider>
     <Routes>
-      <Route path="/" element={<Layout />} >
+      <Route path="/" element={<MainLayout />} >
       <Route index element={<Home />} />
+      </Route> 
       <Route path="/SignIn" element={<SignIn/>} />
       <Route path="/SignUp" element={<SignUp/>} />
-      </Route> 
-      {/* <Route path="/pricing" element={<Pricing />} /> */}
     </Routes>
     </UserContextProvider>
     </ToggleProvider>
--- chatbot-frontend/src/Layout.jsx
+++ chatbot-frontend/src/Layout.jsx
@@ -1,15 +1,19 @@
-import { Outlet } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Footer from "./components/Footer";
+import { useContext } from "react";
+import { Outlet, Navigate } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 export default function Layout() {
+  const { isLoggedIn } = useContext(UserContext);
+
+  if (!isLoggedIn()) {
+    return <Navigate to="/SignIn" replace />;
+  }
+
   return (
     <div className="flex flex-col h-full w-full">
-     
       <main className="flex-1 w-full h-full">
         <Outlet />
       </main>
-      {/* <Footer /> */}
     </div>
   );
-};
+}
