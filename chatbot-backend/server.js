0c4326ad6c9984a281efa1b5873d452b9b2b3aed
Zaim211 <yousseftitazaim@gmail.com>
2024-11-20 21:22:23 +0000

    Add infos for each leads

--- chatbot-backend/server.js
+++ chatbot-backend/server.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 // middleware to connect with frontend
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5175"],
+    origin: ["http://localhost:5173", "http://localhost:5175", "https://chat-bot-dashboard-bice.vercel.app"],
     credentials: true,
 }));
 
