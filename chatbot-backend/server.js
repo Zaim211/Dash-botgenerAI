5a0e9f0fc61dcfc17fb5cd14922b1ceea99f6349
Zaim211 <yousseftitazaim@gmail.com>
2024-11-21 13:19:03 +0000

    Add frontend of chatbot

--- chatbot-backend/server.js
+++ chatbot-backend/server.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 // middleware to connect with frontend
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5175", "https://chat-bot-dashboard-bice.vercel.app"],
+    origin: ["http://localhost:5173", "https://chatbot-ai-six-psi.vercel.app", "https://chat-bot-dashboard-bice.vercel.app"],
     credentials: true,
 }));
 
