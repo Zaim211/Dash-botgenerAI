f817c2aeb95e05d7ce748946f7329c89166f1773
Zaim211 <yousseftitazaim@gmail.com>
2024-11-21 15:39:29 +0000

    Modify stying of table

--- chatbot-frontend/src/pages/Leads.jsx
+++ chatbot-frontend/src/pages/Leads.jsx
@@ -8,9 +8,17 @@ import {
   Select,
   Input as AntdInput,
   Button,
+  Popconfirm,
+  Space,
+  message,
 } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  EditOutlined,
+  DeleteOutlined,
+  UploadOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
 
 const { Option } = Select;
 
@@ -23,6 +31,7 @@ const Leads = () => {
   const [showSpinner, setShowSpinner] = useState(false);
   const [selectedLeads, setSelectedLeads] = useState([]);
   const navigate = useNavigate();
+  const { id } = useParams()
 
   const handlePageChange = (value) => {
     setCurrentPage(value);
@@ -54,6 +63,20 @@ const Leads = () => {
     // Optionally, you could save this change to the backend.
   };
 
+  const handleDelete = async (id) => {
+    try {
+      const response = await axios.delete(`/lead/${id}`)
+
+      console.log("Chat deleted successfully:", response.data);
+      setChatData(chatData.filter((lead) => lead._id !== id)); 
+      message.success("Coach deleted successfully");
+    } catch (error) {
+      console.error("Error deleting coach:", error);
+      message.error("Failed to delete coach");
+    }
+  }
+
+
   // Define table columns with NOM and EMAIL integrated
   const columns = [
     {
@@ -63,7 +86,7 @@ const Leads = () => {
         <div className="cursor-pointer"
         onClick={() => handleCoachClick(record)}>
           <div>{record.request_name || "-"}</div>
-           <div className="text-gray-500 text-sm">
+           <div className="text-gray-500 text-xs">
           {record.verification_email === "Non"
             ? record.request_add_email || "-" 
             : record.request_email || "-"}    
@@ -84,7 +107,8 @@ const Leads = () => {
           minute: "2-digit",
         });
         return (
-          <div>
+          <div className="cursor-pointer"
+          onClick={() => handleCoachClick(record)}>
             <div>{day}</div>
             <div className="text-gray-500 text-sm">{time}</div>
           </div>
@@ -116,7 +140,7 @@ const Leads = () => {
       render: (text, record) => (
         <Select
           defaultValue="nouveau"
-          style={{ width: 120 }}
+          style={{ width: 80 }}
           onChange={(value) => handleStatusLeadChange(value, record)}
         >
           <Option value="nouveau">Nouveau</Option>
@@ -129,12 +153,15 @@ const Leads = () => {
       title: "SPECIALITY",
       dataIndex: "choose_course",
       key: "choose_course",
-      render: (text, record) =>
-        record.program_interest ||
+      render: (text, record) => (
+        <div className="text-gray-500 text-xs">
+       { record.program_interest ||
         record.employee_training ||
         record.choose_course ||
         record.choose_course_salarie ||
-        "-",
+        "-"},
+        </div>
+      ),
     },
     {
       title: "Duration",
@@ -147,10 +174,29 @@ const Leads = () => {
       key: "status",
       render: (text, record) => record.remmberme || record.not_talk || "-",
     },
+    {
+      title: <span style={{ fontSize: "12px" }}>Action</span>,
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          <Popconfirm
+            title="Are you sure you want to delete this coach?"
+            onConfirm={() => handleDelete(record._id)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button
+              icon={<DeleteOutlined />}
+              style={{ backgroundColor: "red", color: "white" }}
+              danger
+              size="small"
+            />
+          </Popconfirm>
+        </Space>
+      ),
+    },
   ];
 
-  // if (loading) return <Spin tip="Loading..." />;
-  // Simulate loading delay
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowSpinner(true);
